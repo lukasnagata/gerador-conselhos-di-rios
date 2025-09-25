aab74241bc0d446491e403e6069feea7baf5fe50
Enzo Lucas <enzo.lucas.santos0820@gmail.com>
2025-09-24 21:50:45 -0300

    feat: Adicionar interatividade ao botão de gerar conselho

---
+++ script.js
@@ -0,0 +1,9 @@
+const conselhos = [
+    "Acredite em você mesmo!",
+    "Você é top!",
+    "O primeiro passo é mais o importante!",
+    "A persistência realiza o impossível!",
+    "Seja gentil com todos que encontrar!"
+];
+
+console.log('Arquivo de script carregado. Array de conselhos pronto.')
